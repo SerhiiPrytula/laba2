a6808caffc7a2894fc3bba8fcf8fd54cb3bdb9bf
серго <@DESKTOP-5O7KUFO>
2021-12-05 15:53:56 +0300

    Добавьте файлы проекта.

---
+++ laba2/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿
+namespace laba2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_P = new System.Windows.Forms.TextBox();
+            this.textBox_Key = new System.Windows.Forms.TextBox();
+            this.textBox_C = new System.Windows.Forms.TextBox();
+            this.button1_XOR = new System.Windows.Forms.Button();
+            this.button_clean = new System.Windows.Forms.Button();
+            this.textBox1_c = new System.Windows.Forms.TextBox();
+            this.textBox2_key = new System.Windows.Forms.TextBox();
+            this.textBox3_p = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(61, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "P =";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(26, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Key = ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(61, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 40);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "C =";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox_P
+            // 
+            this.textBox_P.Location = new System.Drawing.Point(173, 23);
+            this.textBox_P.Name = "textBox_P";
+            this.textBox_P.Size = new System.Drawing.Size(269, 31);
+            this.textBox_P.TabIndex = 3;
+            // 
+            // textBox_Key
+            // 
+            this.textBox_Key.Location = new System.Drawing.Point(173, 61);
+            this.textBox_Key.Name = "textBox_Key";
+            this.textBox_Key.Size = new System.Drawing.Size(269, 31);
+            this.textBox_Key.TabIndex = 4;
+            // 
+            // textBox_C
+            // 
+            this.textBox_C.Location = new System.Drawing.Point(173, 99);
+            this.textBox_C.Name = "textBox_C";
+            this.textBox_C.Size = new System.Drawing.Size(269, 31);
+            this.textBox_C.TabIndex = 5;
+            // 
+            // button1_XOR
+            // 
+            this.button1_XOR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1_XOR.Location = new System.Drawing.Point(173, 146);
+            this.button1_XOR.Name = "button1_XOR";
+            this.button1_XOR.Size = new System.Drawing.Size(100, 50);
+            this.button1_XOR.TabIndex = 6;
+            this.button1_XOR.Text = "XOR";
+            this.button1_XOR.UseVisualStyleBackColor = true;
+            this.button1_XOR.Click += new System.EventHandler(this.button1_XOR_Click);
+            // 
+            // button_clean
+            // 
+            this.button_clean.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_clean.Location = new System.Drawing.Point(279, 146);
+            this.button_clean.Name = "button_clean";
+            this.button_clean.Size = new System.Drawing.Size(165, 50);
+            this.button_clean.TabIndex = 7;
+            this.button_clean.Text = "Прибрати";
+            this.button_clean.UseVisualStyleBackColor = true;
+            this.button_clean.Click += new System.EventHandler(this.button_clean_Click);
+            // 
+            // textBox1_c
+            // 
+            this.textBox1_c.Location = new System.Drawing.Point(610, 23);
+            this.textBox1_c.Name = "textBox1_c";
+            this.textBox1_c.Size = new System.Drawing.Size(250, 31);
+            this.textBox1_c.TabIndex = 8;
+            // 
+            // textBox2_key
+            // 
+            this.textBox2_key.Location = new System.Drawing.Point(610, 61);
+            this.textBox2_key.Name = "textBox2_key";
+            this.textBox2_key.Size = new System.Drawing.Size(250, 31);
+            this.textBox2_key.TabIndex = 9;
+            // 
+            // textBox3_p
+            // 
+            this.textBox3_p.Location = new System.Drawing.Point(610, 99);
+            this.textBox3_p.Name = "textBox3_p";
+            this.textBox3_p.Size = new System.Drawing.Size(250, 31);
+            this.textBox3_p.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(318, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 30);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "2";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(411, 256);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 30);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "10";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(512, 256);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 30);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "16";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(327, 286);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(24, 25);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(430, 286);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(24, 25);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(531, 286);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(24, 25);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "0";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(272, 217);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(412, 25);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Результат обчислень у різних системах";
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(504, 11);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(90, 40);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "C =";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(475, 53);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(119, 40);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Key = ";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(514, 91);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(90, 40);
+            this.label13.TabIndex = 18;
+            this.label13.Text = "P =";
+            this.label13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 344);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3_p);
+            this.Controls.Add(this.textBox2_key);
+            this.Controls.Add(this.textBox1_c);
+            this.Controls.Add(this.button_clean);
+            this.Controls.Add(this.button1_XOR);
+            this.Controls.Add(this.textBox_C);
+            this.Controls.Add(this.textBox_Key);
+            this.Controls.Add(this.textBox_P);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_P;
+        private System.Windows.Forms.TextBox textBox_Key;
+        private System.Windows.Forms.TextBox textBox_C;
+        private System.Windows.Forms.Button button1_XOR;
+        private System.Windows.Forms.Button button_clean;
+        private System.Windows.Forms.TextBox textBox1_c;
+        private System.Windows.Forms.TextBox textBox2_key;
+        private System.Windows.Forms.TextBox textBox3_p;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ laba2/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace laba2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_XOR_Click(object sender, EventArgs e)
+        {
+            if (textBox_P.Text != "" && textBox_Key.Text != "") {
+                string strP = textBox_P.Text;
+                int intP = Convert.ToInt32(strP, 16);
+                string strKey = textBox_Key.Text;
+                int intKey = Convert.ToInt32(strKey, 16);
+
+                int intC = intP ^ intKey;
+                string strC = Convert.ToString(intC, 16);
+                textBox_C.Text = strC;
+
+                label7.Text = Convert.ToString(intC, 2);
+                label8.Text = Convert.ToString(intC, 10);
+                label9.Text = Convert.ToString(intC, 16);
+
+                textBox1_c.Text = strC;
+                textBox2_key.Text = strKey;
+                textBox3_p.Text = strP;
+            }
+            else
+            {
+                MessageBox.Show("Помилка: Введіть змінні Р та key");
+            }
+
+        }
+
+        private void button_clean_Click(object sender, EventArgs e)
+        {
+            textBox_P.Text = "";
+            textBox_Key.Text = "";
+            textBox_C.Text = "";
+            textBox1_c.Text = "";
+            textBox2_key.Text = "";
+            textBox3_p.Text = "";
+
+            label7.Text = "0";
+            label8.Text = "0";
+            label9.Text = "0";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
